1c69406c8a4cdac209db0a48f70ae3b0e3a997fe
MariamNsubuga <mariam.nakanyike@gmail.com>
2018-09-17 18:07:55 +0300

    modify age

--- age.py
+++ age.py
@@ -1,14 +1,14 @@
 import datetime
 from datetime import date
-birthyear = int(input("enter your year of birth"))
+birthyear = int(input("enter your year of birth:"))
 currentyear = int(datetime.date.today().strftime("%Y"))
 useyear = currentyear-birthyear
 def bB(useyear):
     if len(str(birthyear)) != 4:
         print("invalid year of birth") 
-    if useyear < 18:
+    if  useyear < 18:
         print("your are minor")
-    if (useyear >= 18  and useyear <= 35):
+    if  (useyear >= 18  and useyear <= 35):
         print("your are a youth")
     if useyear > 35:
         print("your are an elder")
